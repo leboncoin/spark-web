13d890758946aebff34fa318548e47092298428d
Powerplex <julien.bonnin.dev@gmail.com>
2025-04-03 10:11:43 +0200

    chore: updated all remaining deps to latest version

--- packages/utils/internal-utils/vite.config.ts
+++ packages/utils/internal-utils/vite.config.ts
@@ -1,26 +1,29 @@
 import terser from '@rollup/plugin-terser'
 import dts from 'vite-plugin-dts'
-import browserslistToEsbuild from 'browserslist-to-esbuild'
 
 const pkg = require('./package.json')
 const peerDeps = Object.keys(pkg.peerDependencies || {})
 
-export default {
-  build: {
-    target: browserslistToEsbuild(),
-    lib: {
-      entry: 'src/index.ts',
-      formats: ['es', 'cjs'],
-      fileName: 'index',
+export default async () => {
+  const browserslistToEsbuild = await import('browserslist-to-esbuild')
+
+  return {
+    build: {
+      target: browserslistToEsbuild.default(),
+      lib: {
+        entry: 'src/index.ts',
+        formats: ['es', 'cjs'],
+        fileName: 'index',
+      },
+      rollupOptions: {
+        external: ['node:path', 'node:fs', ...peerDeps],
+        plugins: [terser()],
+      },
     },
-    rollupOptions: {
-      external: ['node:path', 'node:fs', ...peerDeps],
-      plugins: [terser()],
-    },
-  },
-  plugins: [
-    dts({
-      entryRoot: './src',
-    }),
-  ],
+    plugins: [
+      dts({
+        entryRoot: './src',
+      }),
+    ],
+  }
 }
