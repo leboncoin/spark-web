61541f518ec433b8af2ad9ee4f5115be41b6dca6
Powerplex <julien.bonnin.dev@gmail.com>
2025-05-13 15:10:04 +0200

    refactor(components): migrated collapsible from radix to base-ui

---
+++ packages/components/src/collapsible/Content.tsx
@@ -0,0 +1,37 @@
+import { Collapsible } from '@base-ui-components/react/collapsible'
+import { cx } from 'class-variance-authority'
+import { type ComponentProps } from 'react'
+
+import { useRenderSlot } from './useRenderSlot'
+
+export interface ContentProps extends ComponentProps<typeof Collapsible.Panel> {
+  /**
+   * Change the default rendered element for the one passed as a child, merging their props and behavior.
+   */
+  asChild?: boolean
+}
+
+export const Content = ({ asChild = false, className, children, ...props }: ContentProps) => {
+  const renderSlot = useRenderSlot(asChild, 'div')
+
+  return (
+    <Collapsible.Panel
+      data-spark-component="collapsible-content"
+      className={cx(
+        'overflow-hidden',
+        'h-[var(--collapsible-panel-height)]',
+        'transition-all duration-200',
+        'motion-reduce:transition-none',
+        'data-[starting-style]:h-0',
+        'data-[ending-style]:h-0',
+        className
+      )}
+      render={renderSlot}
+      {...props}
+    >
+      {children}
+    </Collapsible.Panel>
+  )
+}
+
+Content.displayName = 'Collapsible.Content'
