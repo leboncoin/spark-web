10f344b77a6df8b166da75f5bb1c6fb8df306a7f
jean-paul.lescouzeres <jean-paul.lescouzeres@adevinta.com>
2025-05-16 12:54:53 +0200

    feat(components): improves stepper empty value behavior with min/max limits

--- packages/components/src/stepper/useStepper.ts
+++ packages/components/src/stepper/useStepper.ts
@@ -16,6 +16,18 @@ export const useStepper = ({
     locale,
   })
 
+  /**
+   * React Arias useNumberFieldState does not support the cases where value is unset (empty) and min/max constraints the value to positive or negative values only.
+   * Cf: https://github.com/adobe/react-spectrum/blob/b7f8ed1a9a311d89719b1a746b0a58204841b21a/packages/%40react-stately/numberfield/src/useNumberFieldState.ts#L251C3-L260C76
+   */
+  const canDecrement =
+    !(isNaN(state.numberValue) && state.minValue !== undefined && state.minValue >= 0) &&
+    state.canDecrement
+
+  const canIncrement =
+    !(isNaN(state.numberValue) && state.maxValue !== undefined && state.maxValue <= 0) &&
+    state.canIncrement
+
   const { groupProps, inputProps, incrementButtonProps, decrementButtonProps } = useNumberField(
     {
       isWheelDisabled: false,
@@ -24,7 +36,11 @@ export const useStepper = ({
       isReadOnly: rest.readOnly,
       isRequired: rest.required,
     },
-    state,
+    {
+      ...state,
+      canDecrement,
+      canIncrement,
+    },
     inputRef
   )
 
