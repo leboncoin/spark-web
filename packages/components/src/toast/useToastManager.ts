8c8d6be045718482a1e86a856ede3b10c9474834
Powerplex <julien.bonnin.dev@gmail.com>
2025-09-03 18:25:27 +0200

    feat(components): toast component

---
+++ packages/components/src/toast/useToastManager.ts
@@ -0,0 +1,17 @@
+import { Toast as BaseToast } from '@base-ui-components/react/toast'
+import * as React from 'react'
+
+import type { UseToastManagerReturnValue } from './types'
+
+export function useToastManager(): UseToastManagerReturnValue {
+  const baseToastManager = BaseToast.useToastManager()
+
+  const closeAll = React.useCallback((): void => {
+    baseToastManager.toasts.forEach(({ id }) => baseToastManager.close(id))
+  }, [baseToastManager])
+
+  return {
+    ...baseToastManager,
+    closeAll,
+  }
+}
