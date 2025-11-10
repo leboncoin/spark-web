9556cad71731c209b9b6f98bf0f1e411eae8884e
Abeba NGWE <abeba.ngwe@adevinta.com>
2025-11-10 12:28:08 +0100

    feat(components): add popups to Tabs

--- packages/components/src/tabs/index.ts
+++ packages/components/src/tabs/index.ts
@@ -1,24 +1,29 @@
 import { Tabs as Root } from './Tabs'
 import { TabsContent as Content } from './TabsContent'
 import { TabsList as List } from './TabsList'
+import { TabsPopupWrapper as Popup } from './TabsPopupWrapper'
 import { TabsTrigger as Trigger } from './TabsTrigger'
 
 export const Tabs: typeof Root & {
   List: typeof List
   Trigger: typeof Trigger
   Content: typeof Content
+  Popup: typeof Popup
 } = Object.assign(Root, {
   List,
   Trigger,
   Content,
+  Popup,
 })
 
 Tabs.displayName = 'Tabs'
 List.displayName = 'Tabs.List'
 Trigger.displayName = 'Tabs.Trigger'
 Content.displayName = 'Tabs.Content'
+Popup.displayName = 'Tabs.Popup'
 
 export { type TabsContentProps } from './TabsContent'
 export { type TabsListProps } from './TabsList'
+export { type TabsPopupWrapperProps as TabsPopupProps } from './TabsPopupWrapper'
 export { type TabsProps, type TabsRootProps } from './Tabs'
 export { type TabsTriggerProps } from './TabsTrigger'
